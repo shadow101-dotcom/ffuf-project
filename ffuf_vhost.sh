408ae6028137ad9d822381063216e4b8781f3363
shadow101-dotcom <shadyawad12443@gmail.com>
2026-04-08 01:24:21 +0200

    Modify ffuf_vhost.sh to use domain argument
    Updated script to accept domain as an argument and changed wordlist path.

--- ffuf_vhost.sh
+++ ffuf_vhost.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-TARGET="http://example.com"
-WORDLIST="/usr/share/wordlists/subdomains-top1million-5000.txt"
+DOMAIN=$1
+WORDLIST="/usr/share/wordlists/seclists/Discovery/DNS/subdomains-top1million-5000.txt"
 
-ffuf -u $TARGET -H "Host: FUZZ.example.com" -w $WORDLIST -mc 200 -o vhost_results.txt -v
+if [ -z "$DOMAIN" ]; then
+    echo "Usage: $0 <domain.com>"
+    exit 1
+fi
+
+ffuf -u "http://$DOMAIN" -H "Host: FUZZ.$DOMAIN" -w "$WORDLIST" -mc 200 -o vhost_results.txt -v
